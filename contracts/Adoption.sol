7f3a9e600a49008088845ee4f6ec7432ce09248c
Isaac Ikusika <helloayodeji@gmail.com>
2021-11-08 11:35:15 +0100

    added adoption contract and test file

---
+++ contracts/Adoption.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >= 0.5.0 < 0.9.0;
+
+contract Adoption{
+    address[16] public adopters;
+
+    function requestAdoption(uint petId) public returns (uint){
+        
+        require(petId > 0 && petId < 15);
+
+        adopters[petId] = msg.sender;
+
+        return petId;
+    }
+
+    function getAdoptions() public view returns (address[16] memory) {
+        return adopters;
+    }
+}
